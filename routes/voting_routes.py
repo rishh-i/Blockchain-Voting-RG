1caad48f3f4707b2f3ebdf3dd8bba57e030514de
rishh-i <rishigupta2210@gmail.com>
2025-08-14 14:39:49 +0100

    voting page and submit vote functions
    submit vote does not run because some of app.py is incomplete.
    will edit app.py soon

--- routes/voting_routes.py
+++ routes/voting_routes.py
@@ -38,3 +38,97 @@ def dashboard():
                            is_admin = current_user.is_admin,)
 
 
+@voting_bp.route("/election/<int:election_id>")
+@login_required
+def voting_page(election_id):
+    election = Election.query.get_or_404(election_id)
+    now = datetime.now(timezone.utc)
+
+    # general checks for election status
+    if election.start_date > now:
+        flash("This election has not started yet.", "error")
+        return redirect(url_for("voting.dashboard"))
+    if election.end_date and election.end_date < now:
+        flash("This election has ended.", "error")
+        return redirect(url_for("voting.dashboard"))
+
+    #checks if user has already voted in this election
+    existing_vote = VoteRecord.query.filter_by(
+        voter_id=current_user.voter_id,
+        election_id=election.id
+    ).first()
+    if existing_vote:
+        flash("You have already voted in this election.", "error")
+        return redirect(url_for("voting.dashboard"))
+
+    candidates = Candidate.query.filter_by(
+        election_id=election.id
+    ).all()
+
+    return render_template("vote.html", election = election, candidates = candidates)
+
+
+@voting_bp.route("/submit_vote", methods=["POST"])
+@login_required
+# some steps are repeated throughout the routes e.g. checking if user has already voted
+def submit_vote():
+    election_id = request.form.get("election_id", type=int)
+    candidate_id = request.form.getlist("candidate_id", type=int)
+
+    if not election_id or not candidate_id:
+        flash("Invalid submission.", "error")
+        return redirect(url_for("voting.dashboard"))
+
+    election = Election.query.get_or_404(election_id)
+    candidate = Candidate.query.filter_by(
+        id=candidate_id,
+        election_id=election.id
+    ).first()
+
+    if not candidate:
+        flash("Invalid candidate selection.", "error")
+        return redirect(url_for("voting.voting_page", election_id=election_id))
+
+    existing_vote = VoteRecord.query.filter_by(
+        voter_id=current_user.voter_id,
+        election_id=election.id
+    ).first()
+
+    if existing_vote:
+        flash("You have already voted in this election.", "error")
+        return redirect(url_for("voting.dashboard"))
+
+    # will use blockchain logic imported from folder blockchain_logic to cast vote
+    try:
+        bc_vote = Vote(
+            voter_id=current_user.voter_id,
+            election_id=election.id,
+            candidate_id=candidate.id
+        )
+
+        current_app.blockchain.add_vote(bc_vote) #adds vote to blockchain
+
+        vote_record = VoteRecord(
+            voter_id=current_user.voter_id,
+            election_id=election.id
+        )
+        db.session.add(vote_record)  # adds vote to database
+        db.session.commit()
+
+        flash(f"Vote submitted for {candidate.name}", "success")
+
+    except ValueError as e:
+        flash(f"Vote submission failed: {str(e)}", "error")
+        return redirect(url_for("voting.voting_page", election_id=election_id))
+
+    except Exception as e:
+        db.session.rollback()
+        flash("An error occurred while submitting your vote", "error")
+        return redirect(url_for("voting.voting_page", election_id=election_id))
+
+    return redirect(url_for("voting.dashboard"))
+
+
+
+
+
