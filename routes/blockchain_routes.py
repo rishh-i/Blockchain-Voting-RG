efc7b85135def709cd1875d16ae467a079c43a1b
rishh-i <rishigupta2210@gmail.com>
2025-12-27 16:59:13 +0000

    Re-worked BC explorer
    Now all accounts view the same blockchain explorer.
    
    Kept a copy of admin_blockchain_explorer in backup

--- routes/blockchain_routes.py
+++ routes/blockchain_routes.py
@@ -8,15 +8,6 @@ def explorer():
     # displays the blockchain explorer page
     return render_template("blockchain_explorer.html")
 
-@blockchain_bp.route("/admin/full_explorer")
-@login_required
-def admin_full_explorer():
-    # this is for the admin page of the bc explorer; i.e. they have greater access and privileges
-    if not current_user.is_admin:
-        flash("Admin access required", "error")
-        return redirect(url_for("voting.dashboard"))
-    return render_template("admin_blockchain_explorer.html")
-
 @blockchain_bp.route("/api/chain")
 @login_required
 def get_chain():
@@ -59,19 +50,20 @@ def get_block(block_index):
             block = chain[block_index]
             block_data = block.to_dict()
 
-            if not current_user.is_admin:
-                anonymous_votes = []
-                for vote in block_data["votes"]:
-                    anonymous_vote = {
-                        "candidate_id": vote["candidate_id"],
-                        "election_id": vote["election_id"],
-                        "timestamp": vote["timestamp"],
-                        "vote_hash": vote["vote_hash"],
-                        "voter_id_hidden": True
-                    }
-                    anonymous_votes.append(anonymous_vote)
-                block_data["votes"] = anonymous_votes
-                block_data["anonymous_view"] = True
+            # if not current_user.is_admin:
+            # from lines 55-67, code was in a loop which excluded admins. Now everyone has the normal bc.
+            anonymous_votes = []
+            for vote in block_data["votes"]:
+                anonymous_vote = {
+                    "candidate_id": vote["candidate_id"],
+                    "election_id": vote["election_id"],
+                    "timestamp": vote["timestamp"],
+                    "vote_hash": vote["vote_hash"],
+                    "voter_id_hidden": True
+                }
+                anonymous_votes.append(anonymous_vote)
+            block_data["votes"] = anonymous_votes
+            block_data["anonymous_view"] = True
             return jsonify(block_data)
         else:
             return jsonify({"error": "Block not found"}), 404
@@ -136,21 +128,41 @@ def validate_vote():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@blockchain_bp.route("api/admin/full_chain")
-@login_required
-def get_full_chain():
 
-    #only admin account can access full chain with voter details
-    if not current_user.is_admin:
-        return jsonify({"error": "Admin access required"}), 403
+"""
+Below are routes that give more detailed data on the blockcain, mainly showing voter IDs
+It can be used in general since voter IDs are random strings but currently the id is hidden
+When these routes were used, they were admin routes only
 
-    try:
-        chain_data = current_app.blockchain.get_blockchain()
-        return jsonify({
-            "chain": chain_data,
-            "length": len(chain_data),
-            "is_valid": current_app.blockchain.validate_chain(),
-            "admin_view": True
-        })
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+Note: if i want to re-use the routes, html changes need to be made:
+- admin_blockchain_explorer.html needs to be re-created (kept a copy)
+- minor changes (buttons) on blockchain_explorer.html and dashboard.html
+"""
+
+# @blockchain_bp.route("api/admin/full_chain")
+# @login_required
+# def get_full_chain():
+#
+#     #only admin account can access full chain with voter details
+#     if not current_user.is_admin:
+#         return jsonify({"error": "Admin access required"}), 403
+#
+#     try:
+#         chain_data = current_app.blockchain.get_blockchain()
+#         return jsonify({
+#             "chain": chain_data,
+#             "length": len(chain_data),
+#             "is_valid": current_app.blockchain.validate_chain(),
+#             "admin_view": True
+#         })
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
+
+# @blockchain_bp.route("/admin/full_explorer")
+# @login_required
+# def admin_full_explorer():
+#     # this is for the admin page of the bc explorer; i.e. they have greater access and privileges
+#     if not current_user.is_admin:
+#         flash("Admin access required", "error")
+#         return redirect(url_for("voting.dashboard"))
+#     return render_template("admin_blockchain_explorer.html")
